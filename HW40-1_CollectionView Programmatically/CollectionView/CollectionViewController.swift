23a981945f26e45b0fcf9ef4e15e49ddac43c49f
Daweihao84 <dwsamurai84@gmail.com>
2023-12-23 01:22:23 +0800

    No changes

--- HW40-1_CollectionView Programmatically/CollectionView/CollectionViewController.swift
+++ HW40-1_CollectionView Programmatically/CollectionView/CollectionViewController.swift
@@ -29,7 +29,7 @@ class CollectionViewController: UIViewController {
 
             let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
             collectionView.backgroundColor = .systemBackground
-            collectionView.alwaysBounceVertical = true
+
             return collectionView
         }()
 
