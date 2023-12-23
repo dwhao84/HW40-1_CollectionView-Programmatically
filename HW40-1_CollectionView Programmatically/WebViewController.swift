aea430252259a3610716c362ca580dcf3fdbd8e9
Daweihao84 <dwsamurai84@gmail.com>
2023-12-23 16:55:12 +0800

    2023/12/23: 1. Add activityIndicatorView in webview.

--- HW40-1_CollectionView Programmatically/WebViewController.swift
+++ HW40-1_CollectionView Programmatically/WebViewController.swift
@@ -10,41 +10,77 @@ import WebKit
 
 class WebViewController: UIViewController {
 
-    var webView: WKWebView!
+//    var webView = WKWebView()
     var url: String?
+    var activityIndicator = UIActivityIndicatorView()
 
     private let backButton: UIButton = {
-        let backButton = UIButton(type: .system)
-        var config = UIButton.Configuration.tinted()
-        var title = AttributedString("Back")
-        title.font = UIFont.systemFont(ofSize: 10)
+        let backButton                    = UIButton(type: .system)
+        var config                        = UIButton.Configuration.tinted()
+        var title                         = AttributedString("Back")
+        title.font                        = UIFont.systemFont(ofSize: 10)
         config.background.backgroundColor = .systemBlue
-        config.attributedTitle = title
-        config.baseBackgroundColor = .systemBlue
-        backButton.configuration = config
+        config.attributedTitle            = title
+        config.baseBackgroundColor        = .systemBlue
+        backButton.configuration          = config
         return backButton
     }()
 
+    private var webView = {
+        // showing webpage styling
+        let prefs = WKWebpagePreferences()
+        // Enable to see the JS content.
+        prefs.allowsContentJavaScript           = true
+        let configuration                       = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = prefs
+        let webView                             = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+
+        let activityIndicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        activityIndicatorView.style            = .medium
+        activityIndicatorView.color            = .gray
+        activityIndicatorView.hidesWhenStopped = true
+
+        let barButton = UIBarButtonItem(customView: activityIndicator)
+        self.navigationItem.setRightBarButton(barButton, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
-
-        let myURL = URL(string: url!)
+        let myURL     = URL(string: url!)
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
+        webView.customUserAgent = ""   // The custom user agent string.
 
-        print("Into the WebViewController")
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) { self.webView.evaluateJavaScript("document.body.innerHTML") { result, error in
+            guard let html = result as? String, error == nil else { return }
+            // print out the html content
+            print(html)
+        }
+            print("Into the WebViewController")
+        }
     }
 
+    func showActivityIndicator(show: Bool) {
+            if show {
+                // Start the loading animation
+                activityIndicator.startAnimating()
+            } else {
+                // Stop the loading animation
+                activityIndicator.stopAnimating()
+            }
+        }
 
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        view = webView
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("didReceiveMemoryWarning")
     }
 }
 
@@ -56,11 +92,25 @@ extension WebViewController: UIWebViewDelegate, WKNavigationDelegate, WKUIDelega
         print("webViewDidClose")
     }
 
+    // WKNavigationDelegate
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        backButton.isHidden = !webView.canGoBack
+        print("Finish to load")
+        title = webView.title
+        showActivityIndicator(show: false)
     }
 
-    // WKNavigationDelegate
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("Start to load")
+        showActivityIndicator(show: true)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print(error.localizedDescription)
+        showActivityIndicator(show: false)
+    }
+
+
+
     func goBack() {
 
     }
